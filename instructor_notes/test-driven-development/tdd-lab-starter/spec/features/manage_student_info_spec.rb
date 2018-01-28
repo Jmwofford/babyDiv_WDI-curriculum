6a022268a299ec834a41fc0bec46b01dc5e46c4e
dphurley <hurley.dannyp@gmail.com>
2018-01-28 10:07:35 -0500

    Pushing up starter code for TDD lesson.

---
+++ instructor_notes/test-driven-development/tdd-lab-starter/spec/features/manage_student_info_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'managing user information', :type => :feature do
+
+  it 'shows all users on the home page' do
+    #   run 'rspec' in the console to get started
+  end
+
+end
