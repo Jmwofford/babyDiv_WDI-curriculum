92b674c60cd2cdd4144a2efc46399d0fdcea0ae9
dphurley <hurley.dannyp@gmail.com>
2018-01-28 10:01:23 -0500

    Added some comments to TDD lab solution.

--- instructor_notes/test-driven-development/tdd-lab-solution/spec/features/manage_student_info_spec.rb
+++ instructor_notes/test-driven-development/tdd-lab-solution/spec/features/manage_student_info_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'managing user information', :type => :feature do
 
-  before :each do
-
-  end
-
   it 'shows all users on the home page' do
 
     given_the_database_is_empty
@@ -20,20 +16,34 @@ RSpec.describe 'managing user information', :type => :feature do
   private
 
   def given_i_am_on_the_home_page
+
+    # Visit the /users page in a browser
+
     visit '/users'
   end
 
   def when_i_view_the_user_display_area
+
+    # expect the users display wrapper div is on the screen
+
     user_display = page.find('#users-display')
     expect(user_display).to be_truthy
   end
 
   def then_i_should_see_two_users_on_the_page
+
+    # expect two individual user divs are on the screen
+
     users = page.find_all('#user')
     expect(users.length).to equal 2
   end
 
   def then_i_should_see_the_first_name_last_name_and_age_for_each_user
+
+    # spot-check the user divs to make sure they are rendering the correct data:
+    # it's fine to just check one of them since our unit tests will make sure
+    # everything else is happening correctly before they render
+
     users = page.find_all('#user')
     first_user = users.first
 
@@ -43,10 +53,16 @@ RSpec.describe 'managing user information', :type => :feature do
   end
 
   def given_the_database_is_empty
+
+    # clear the database
+
     User.delete_all
   end
 
   def given_there_are_two_users_created_in_the_database
+
+    # create a couple of test users
+
     User.create!(
         first_name: 'Bob',
         last_name: 'Loblaw',
