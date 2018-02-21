596da8deb588d5e714a9dc2ed037abd3b303ac03
jamieking <james.king@generalassemb.ly>
2018-02-21 09:17:57 -0500

    formatting

--- projects/unit_02/project-two-cheat-codes/db/schema.js
+++ projects/unit_02/project-two-cheat-codes/db/schema.js
@@ -1,35 +1,32 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-// Use native promises
-mongoose.Promise = global.Promise;
+let mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
 var ItemSchema = new Schema({
   name: String
-});
+})
 
 var UserSchema = new Schema({
   first_name: String,
   email: { type: String, required: true, unique: true },
   created_at: Date,
   updated_at: Date,
-  items: [ItemSchema]
-});
+  items: [ ItemSchema ]
+})
 
-UserSchema.pre('save', function(next){
-  now = new Date();
-  this.updated_at = now;
-  if ( !this.created_at ) {
-    this.created_at = now;
+UserSchema.pre('save', function (next) {
+  now = new Date()
+  this.updated_at = now
+  if (!this.created_at) {
+    this.created_at = now
   }
-  next();
-});
+  next()
+})
 
 
-var UserModel = mongoose.model("User", UserSchema);
-var ItemModel = mongoose.model("Item", ItemSchema);
+var UserModel = mongoose.model('User', UserSchema)
+var ItemModel = mongoose.model('Item', ItemSchema)
 
 module.exports = {
   User: UserModel,
   Item: ItemModel
-};
+}
