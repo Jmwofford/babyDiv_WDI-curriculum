5a7c18e9c302303752ab6ef012107f144fc5e59d
jamieking <james.king@generalassemb.ly>
2018-02-23 11:14:34 -0500

    solution

--- instructor_notes/ajax/giphy-app-solution/models/gif.js
+++ instructor_notes/ajax/giphy-app-solution/models/gif.js
@@ -1,5 +1,5 @@
-var Schema = require('../db/schema');
-var mongoose = require('mongoose');
+let Schema = require('../db/schema')
+var mongoose = require('mongoose')
 
-var Gif = Schema.Gif;
-module.exports = Gif;
+var Gif = Schema.Gif
+module.exports = Gif
